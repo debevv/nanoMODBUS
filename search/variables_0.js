c7bd9f1f5326405a55a21fbc5adc8e6873999698
Valerio De Benedetto <debevv@users.noreply.github.com>
2022-06-26 11:12:05 +0200

    Updated docs

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['arg_67',['arg',['../structnmbs__platform__conf.html#abec793041ee3e19d14fede444f85aa06',1,'nmbs_platform_conf']]]
+  ['arg_70',['arg',['../structnmbs__platform__conf.html#abec793041ee3e19d14fede444f85aa06',1,'nmbs_platform_conf']]]
 ];
