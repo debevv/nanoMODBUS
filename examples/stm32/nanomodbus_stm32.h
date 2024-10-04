35e49702b4f3d45f2426041edcb4559a30e39dc2
Donghoon Park <donghun94@gmail.com>
2024-10-04 13:14:23 +0900

    Add handler prototypes and server data object

--- examples/stm32/nanomodbus_stm32.h
+++ examples/stm32/nanomodbus_stm32.h
@@ -7,6 +7,11 @@ extern "C" {
 
 #define NANOMB_UART huart1
 #define RX_BUF_SIZE 256
+
+// Max size of coil and register area
+#define COIL_BUF_SIZE 1024 
+#define REG_BUF_SIZE  2048
+
 // Other hardware configuration have done in *.ioc file
 
 // Core name may vary with your hardware
@@ -14,7 +19,14 @@ extern "C" {
 // NanoModbus include
 #include "nanomodbus.h"
 
-void nanomodbus_rtu_init(nmbs_t* nmbs);
+void nanomodbus_server_init(nmbs_t* nmbs);
+void nanomodbus_server_add(nmbs_server_t* server);
+
+struct tNmbsServer{
+    uint8_t  id;
+    uint8_t  coils[COIL_BUF_SIZE];
+    uint16_t regs[REG_BUF_SIZE];
+}typedef nmbs_server_t;
 
 extern UART_HandleTypeDef NANOMB_UART;
 
