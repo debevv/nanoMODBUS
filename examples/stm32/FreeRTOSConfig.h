09f1f9dd0dff39d6160b02b8a8a4fa43072a14ac
Donghoon Park <donghun94@gmail.com>
2024-12-15 03:37:01 +0000

    manual revision to pass clang-format

--- examples/stm32/FreeRTOSConfig.h
+++ examples/stm32/FreeRTOSConfig.h
@@ -63,7 +63,7 @@ extern uint32_t SystemCoreClock;
 #define configMAX_PRIORITIES (5)
 #endif
 #define configMINIMAL_STACK_SIZE ((unsigned short) 50)
-#define configTOTAL_HEAP_SIZE ((size_t)(32 * 1024))
+#define configTOTAL_HEAP_SIZE (size_t)(32 * 1024)
 #define configMAX_TASK_NAME_LEN (10)
 #define configUSE_TRACE_FACILITY 1
 #define configUSE_16_BIT_TICKS 0
